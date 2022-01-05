51865058479ff34f4de9490974f506911748dd98
Wessel Kuijstermans <w.kuijstermans@home.nl>
2022-01-05 14:27:06 +0100

    JavaFx kan mijn lul zuigen

---
+++ app/Main.java
@@ -0,0 +1,18 @@
+package app;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+class Main extends Application {
+    
+    @Override
+    public void start(Stage window) {
+        window.setTitle("Hello World!");
+        window.show();
+    }
+    public static void main(String[] args) {
+        launch(Main.class);
+    } 
+
+   
+}
